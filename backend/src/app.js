af3abc99452a2703654fee64b48e824f117f4939
Keaton Perry <54960503+keatonperry99@users.noreply.github.com>
2021-04-25 13:56:10 -0700

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,20 +2,18 @@
 var express = require('express');
 var cors = require('cors');
 var app = express();
-var handlebars = require('express-handlebars').create({defaultLayout:'main'});
 var bodyParser = require('body-parser');
 var spawn = require('child_process').spawn;
+var fs = require('fs');
 
 // More dependency setup
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.engine('handlebars', handlebars.engine);
-app.set('view engine', 'handlebars');
 app.set('port', 8030);
 app.use(cors());
 
 // The app.post endpoint that the data from the frontend is sent to
-app.post('/post-loopback', function(req, res) {
+app.post('/', function(req, res) {
 	
 	// Getting the number of questions
 	var numQuestions = 0;
@@ -30,26 +28,17 @@ app.post('/post-loopback', function(req, res) {
 		var process = spawn('python', ["./scraper.py", req.body.query[p][0]]);
 		process.stdout.on('data', function(data) {
 			
-			// Printing the URL
-			console.log(data.toString());
+			// If an actual URL is returned from the scraper
+			if ((data.toString()).length > 5) {
+				fs.writeFile('URLresults.txt', data.toString(), { flag: 'a' }, err => {});
+				// Printing the URL
+				console.log(data.toString());
+			}
 		})
 	}
 });
 
-// Placeholder app.use function for 404 error
-app.use(function(req, res){
-	res.status(404);
-	res.render('404');
-});
-
-// Placeholder app.use function for 500 error
-app.use(function(err, req, res, next){
-	console.error(err.stack);
-	res.type('plain/text');
-	res.status(500);
-	res.render('500');
-});
-
+// Listening on the port
 app.listen(app.get('port'), function(){
 	console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
 });
